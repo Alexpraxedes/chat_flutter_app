b0ff48d2fb591d04ae4f6c6c611308aac6793958
Alex Praxedes Silveira <id.praxedes@gmail.com>
2023-05-22 18:45:37 -0300

    feat: add login page

---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = 'AIzaSyDeeFV-j5eNbtskbR7GHvYHmeQQTdSE6yo';
+  static String apiKey = 'AIzaSyDeeFV-j5eNbtskbR7GHvYHmeQQTdSE6yo';
+  static String messagingSenderId = '382885118832';
+  static String projectId = 'chatapp-fe358';
+}
